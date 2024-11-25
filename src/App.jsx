d458acad7b963b7e2e117b00763d216d936ba2ba
martinbrumana <martinbrumana@gmail.com>
2024-11-24 23:58:14 -0300

    package

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import Layout from "./components/Layout";
+import { Client } from "appwrite";
 
 function App() {
+  const client = new Client();
+  client.setProject("66e24eba003d137710bb");
+  
   return (
     <>
       <Layout />
